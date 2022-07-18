18f9c8b41b6c6deb181755b09fa274cdda15927d
Clay Colwell <claytonraymond2004@gmail.com>
2022-07-18 12:22:34 -0400

    fix: move faye to layout to avoid dup instances

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,6 @@
 </template>
 
 <script>
-const faye = require('faye')
 export default {
   data() {
     return {}
@@ -47,73 +46,6 @@ export default {
   async mounted(ctx) {
     // Get Orders from backend
     await this.$store.dispatch('orders/getOrders')
-    // Setup Faye listeners
-    const fayeClient = new faye.Client('http://localhost:8080/faye')
-
-    // Listen for order patches (cancelled or complete) and remove and alert cook
-    fayeClient.subscribe('/order/patch', (order) => {
-      if (order.complete || order.cancelled) {
-        this.$store.commit('orders/REMOVE_ORDER', order.id)
-        // Alert cook
-        if (order.complete) {
-          this.$dialog.notify.success(
-            `Order ${String(order.orderNumber).padStart(3, '0')} completed!`,
-            { position: 'top-left', timeout: 5000 }
-          )
-        } else {
-          this.$dialog.notify.error(
-            `Order ${String(order.orderNumber).padStart(3, '0')} cancelled!`,
-            { position: 'top-left', timeout: 0 }
-          )
-        }
-      }
-    })
-
-    // Listen for new orders (/order/post)
-    fayeClient.subscribe('/order/post', (order) => {
-      // Alert cook
-      this.$dialog.notify.info(
-        `New order (${String(order.orderNumber).padStart(3, '0')}) received!`,
-        { position: 'top-left', timeout: 5000 }
-      )
-      // Add order to store
-      this.$store.dispatch('orders/addOrder', order)
-    })
-
-    // Listen for orderItem changes
-    // firing, fired, ready
-    fayeClient.subscribe('/orderItem/patch', (orderItem) => {
-      this.$store.commit('orders/CHANGE_ORDERITEM_STATUS', {
-        orderId: orderItem.orderId,
-        orderItemId: orderItem.id,
-        key: 'fired',
-        value: orderItem.fired,
-      })
-      this.$store.commit('orders/CHANGE_ORDERITEM_STATUS', {
-        orderId: orderItem.orderId,
-        orderItemId: orderItem.id,
-        key: 'firedTime',
-        value: orderItem.firedTime,
-      })
-      this.$store.commit('orders/CHANGE_ORDERITEM_STATUS', {
-        orderId: orderItem.orderId,
-        orderItemId: orderItem.id,
-        key: 'firing',
-        value: orderItem.firing,
-      })
-      this.$store.commit('orders/CHANGE_ORDERITEM_STATUS', {
-        orderId: orderItem.orderId,
-        orderItemId: orderItem.id,
-        key: 'firingTime',
-        value: orderItem.firingTime,
-      })
-      this.$store.commit('orders/CHANGE_ORDERITEM_STATUS', {
-        orderId: orderItem.orderId,
-        orderItemId: orderItem.id,
-        key: 'ready',
-        value: orderItem.ready,
-      })
-    })
   },
   methods: {},
 }
